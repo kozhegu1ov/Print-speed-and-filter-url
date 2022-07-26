934ecfcf77b2a58ac88c246d369c081212cb1eba
KojegulovB <75375116+KojegulovB@users.noreply.github.com>
2022-07-26 17:30:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {Route, Routes} from 'react-router-dom'
+import Layout from "./components/UI/Layout/Layout";
+
+const App = () => {
+  return (
+    <Layout>
+      {/*<Routes>*/}
+      {/*  /!*<Route/>*!/   <= Routes will be here */}
+      {/*</Routes>*/}
+    </Layout>
+  );
+};
+
+export default App;
